6abae2e1b28b90f57179d9a38c9105293d8b5cc7
hajin <wlslzz@gmail.com>
2026-06-26 17:38:37 +0900

    feat: refactor login page to use updated refs and improve navigation after login

--- step17-board-front/src/pages/LoginPage.jsx
+++ step17-board-front/src/pages/LoginPage.jsx
@@ -4,34 +4,36 @@ import { useNavigate } from "react-router-dom";
 
 export default () => {
   //로그인 
-  const username = useRef(null);
-  const password = useRef(null);
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
   const [loading, setLoading] = useState(false);
-  const {login} = useAuth();
+  const { login } = useAuth();
   const navigate = useNavigate();
   const [errorMessage, setErrorMessage] = useState(null);
 
   const handleLogin = async () => {
     setLoading(true);
+    const username = usernameRef.current.value;
+    const password = passwordRef.current.value;
     try {
-      await login(username,password);
-      navigate('/api/board/list');
+      await login(username, password);
+      navigate('/');
     } catch (error) {
       setErrorMessage(error.response?.data?.message || '로그인 실패');
-    }finally{
+    } finally {
       setLoading(false);
     }
   };
 
-  return (  <>
-  <div className="container">
-    <h2>로그인 페이지</h2>
-    <div className="login-form">
-        <input type="text" placeholder="아이디 입력" ref={username} />
-        <input type="password" placeholder="비밀번호 입력" ref={password} />
+  return (<>
+    <div className="container">
+      <h2>로그인 페이지</h2>
+      <div className="login-form">
+        <input type="text" placeholder="아이디 입력" ref={usernameRef} />
+        <input type="password" placeholder="비밀번호 입력" ref={passwordRef} />
         <div className="error-message">{errorMessage}</div>
-        {loading? <p>현재 로그인 중입니다</p> : <button type="button" onClick={handleLogin}>로그인</button>}
-        <button type="button" onClick={() => {navigate('/api/auth/signup')}}>회원가입</button>
+        {loading ? <p>현재 로그인 중입니다</p> : <button type="button" onClick={handleLogin}>로그인</button>}
+        <button type="button" onClick={() => { navigate('/api/auth/signup') }}>회원가입</button>
       </div>
     </div>
   </>);
